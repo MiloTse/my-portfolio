86d6ba855668331d6507b65d65cb1e9d48774040
HuacongXie <solarsys@outlook.com>
2025-04-01 21:41:38 -0400

    1.moved components to folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import Introduction from './components/sections/Introduction.jsx'
 import Projects from './components/sections/Projects.jsx'
 import Skills from './components/sections/Skills.jsx'
@@ -8,16 +8,11 @@ import Hero from "./components/hero/Hero.jsx";
 
 function App() {
     return (
-        <div className="App">
-            <Hero />
-            <Introduction />
-            <Projects />
-            <Skills />
-            <Contact />
-            <div className="bg-blue-100 h-screen flex items-center justify-center">
-                <h1 className="text-4xl font-bold text-blue-600">Tailwind CSS is working!</h1>
-            </div>
-        </div>
+        <main className='bg-[#0d182e] text-white'>
+            <Hero/>
+        
+        </main>
+
     );
 }
 
