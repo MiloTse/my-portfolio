184d9da9c8daec0205a68b3583aee9babaf61652
HuacongXie <solarsys@outlook.com>
2025-04-05 19:57:38 -0400

    1.added Language.jsx file 2.added Language.json file 3.import Language component on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'aos/dist/aos.css'
 import Hero from './components/hero/Hero'
 import {useEffect} from "react";
 import Skills from "./components/skills/Skills.jsx";
+import Language from "./components/language/Language.jsx";
 
 export default function App() {
   useEffect(()=>{
@@ -14,6 +15,8 @@ export default function App() {
    <main className='bg-[#0d182a]'>
     <Hero /> 
     <Skills />
+
+     <Language />
    </main>
   )
 }
