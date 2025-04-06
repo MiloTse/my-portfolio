ad1957f2138e48fc8f502d4b59f9cbfb80468d15
HuacongXie <solarsys@outlook.com>
2025-04-05 20:24:57 -0400

    1.adjusted Certificates.jsx 2.used useEffect hook to fetch data from Certificates.json

--- src/components/certificate/Certificates.jsx
+++ src/components/certificate/Certificates.jsx
@@ -1,10 +1,31 @@
+import {useEffect, useState} from "react";
+
 function Certificates() {
-  return (
-    <section id='certificates' className='relative overflow-hidden flex flex-col text-white body-font'>
-        <div className='container flex flex-wrap px-5 py-24 mx-auto items-center'>
-            <h1 data-aos='fade-right' data-aos-delay='500' className='sm:text-4xl text-2xl font-medium title-font mb-2 text-white'>Certificates</h1>
-        </div>
-    </section>
-  )
+    const [certificates, setCertificates] = useState([]);
+    useEffect(() => {
+        fetch('/api/certificates.json')
+            .then((res) => res.json())
+            .then((data) => {
+                setCertificates(data);
+            });
+    })
+    return (
+        <section id='certificates' className='relative overflow-hidden flex flex-col text-white body-font'>
+            <div className='container flex flex-wrap px-5 py-24 mx-auto items-center'>
+                <div
+                    data-aos='fade-up'
+                    data-aos-delay='200'
+                    className='md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pd-10 border-b border-orange-400'>
+                    <h1 data-aos='fade-right' data-aos-delay='500'
+                        className='sm:text-4xl text-2xl font-medium title-font mb-2 text-white '>Certificates</h1>
+                    <p data-aos='fade-right' data-aos-delay='500' className='leading-relaxed text-base'>
+                        {certificates.description}
+                    </p>
+                </div>
+
+            </div>
+        </section>
+    )
 }
+
 export default Certificates;
