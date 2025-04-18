f1bf3cbf71bced345dcb0ec3949d3af6cff27d7a
HuacongXie <solarsys@outlook.com>
2025-04-17 22:08:17 -0400

    1. changed slidesToShow number

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -15,7 +15,7 @@ export default function Projects() {
     const settings = {
         dots: true,
         infinite: true,
-        slidesToShow:3,
+        slidesToShow:2,
         slidesToScroll: 1,
         arrows: true,
         responsive: [
